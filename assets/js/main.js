0d34ebe9da401d4fa76f3033862c9bea394552b7
naz1ne <fabio.sirigu.1995@gmail.com>
2022-10-06 14:37:36 +0200

    add condition

--- assets/js/main.js
+++ assets/js/main.js
@@ -11,24 +11,26 @@ console.log(userAge);
 // Sulla base di queste informazioni dovrà calcolare il prezzo totale del viaggio, secondo queste regole:
 // il prezzo del biglietto è definito in base ai km (0.21 € al km)
 
-const priceTicket = (userKm * 0.21);
+const priceTicket = Number(userKm * 0.21);
 console.log(priceTicket + 'euro');
 
 // va applicato uno sconto del 20% per i minorenni
 // va applicato uno sconto del 40% per gli over 65.
 
-const salesMinors = (priceTicket / 100) * 20;
+const salesMinors = Number(priceTicket / 100) * 20;
 console.log(salesMinors);
 
-const salesOver = (priceTicket / 100) * 65;
+const salesOver = Number(priceTicket / 100) * 40;
 console.log(salesOver);
 
 if (userAge < 18) {
-      const priceTicketMinors = priceTicket - salesMinors;
+      const priceTicketMinors = Number(priceTicket - salesMinors);
       console.log(priceTicketMinors);
 } else if (userAge > 65) {
-      const priceTicketOver = priceTicket - salesOver;
+      const priceTicketOver = Number(priceTicket - salesOver);
       console.log(priceTicketOver);
+} else {
+      console.log(priceTicket);
 }
 
 // L'output del prezzo finale va messo fuori in forma umana (con massimo due decimali, per indicare centesimi sul prezzo).
