3afe8d5cb9ea162f558367e98e3e2561ff76f735
naz1ne <fabio.sirigu.1995@gmail.com>
2022-10-07 09:05:31 +0200

    add correction variab

--- assets/js/main.js
+++ assets/js/main.js
@@ -11,7 +11,7 @@ console.log(userAge);
 // Sulla base di queste informazioni dovrà calcolare il prezzo totale del viaggio, secondo queste regole:
 // il prezzo del biglietto è definito in base ai km (0.21 € al km)
 
-const priceTicket = Number(userKm * 0.21);
+let priceTicket = Number(userKm * 0.21);
 console.log(priceTicket + 'euro');
 
 // va applicato uno sconto del 20% per i minorenni
