3c98ef05445ef242772b43dacc303f22ba560fcc
naz1ne <fabio.sirigu.1995@gmail.com>
2022-10-07 08:44:14 +0200

    add optimized version

--- assets/js/main.js
+++ assets/js/main.js
@@ -24,19 +24,14 @@ const salesOver = Number(priceTicket / 100) * 40;
 console.log(salesOver);
 
 if (userAge < 18) {
-      const priceTicketMinors = Number(priceTicket - salesMinors);
-      console.log(priceTicketMinors.toFixed( 2 ));
-      document.getElementById("final_price").innerHTML = `${priceTicketMinors.toFixed( 2 )} €`;
-
+      finalPrice = Number(priceTicket - salesMinors);
+      console.log(finalPrice.toFixed( 2 ));
 } else if (userAge > 65) {
-      const priceTicketOver = Number(priceTicket - salesOver);
-      console.log(priceTicketOver.toFixed( 2 ));
-      document.getElementById("final_price").innerHTML = `${priceTicketOver.toFixed( 2 )} €`;
-} else {
-      console.log(priceTicket.toFixed( 2 ));
-      document.getElementById("final_price").innerHTML = `${priceTicket.toFixed( 2 )} €`;
-
+      finalPrice = Number(priceTicket - salesOver);
+      console.log(finalPrice.toFixed( 2 ));
 }
+
+document.getElementById("final_price").innerHTML = `${finalPrice.toFixed( 2 )} €`;
 
 // L'output del prezzo finale va messo fuori in forma umana (con massimo due decimali, per indicare centesimi sul prezzo).
 // Questo richiederà un minimo di ricerca, oltre alla ricerca da fare per ricordarsi come calcolare uno sconto (Googlate qulcosa del tipo: come si calcola lo sconto? e cercate una risorsa che vi possa aiutare a capire cosa fare)
